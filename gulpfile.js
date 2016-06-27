8e57aac6c272f31e0c2b771c7300296f78865213
Jeffrey King <nerdpride4life@gmail.com>
2016-06-27 19:26:58 -0400

    Add production pipeline and modify landing page

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require("browser-sync").create();
 var eslint = require('gulp-eslint');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('default', ['styles', 'lint'], function() {
   // place code for your default task here
@@ -23,7 +25,23 @@ gulp.task('lint', function() {
 	.pipe(eslint.format());
 });
 
-// Conver scss files to css
+// Copy HTML for distribution
+gulp.task('copy-html', function() {
+	gulp.src('./templates/**/*.html')
+		.pipe(gulp.dest('dist/templates'));
+});
+
+// Minify image file size 
+gulp.task('image-dist', function() {
+	gulp.src('./static/images/**/*.png')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/images'));
+	gulp.src('./static/images/**/*.jpg')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/images'));
+});
+
+// Convert scss files to css
 gulp.task('styles', function() {
 	gulp.src('./static/sass/**/*.scss')
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
@@ -33,3 +51,14 @@ gulp.task('styles', function() {
 		.pipe(gulp.dest('./static/css'))
 		.pipe(browserSync.stream());
 });
+
+
+// Minify js files
+gulp.task('scripts-dist', function() {
+	gulp.src('./static/js/**/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist/js'));
+});
+
+// Produce production ready code
+gulp.task('dist', ['scripts-dist', 'styles']);
