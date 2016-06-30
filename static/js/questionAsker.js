60ed64788957b7d1013af7e01d1f1f170ccb3384
Jeffrey King <nerdpride4life@gmail.com>
2016-06-30 10:21:37 -0400

    Add email ID feature

--- static/js/questionAsker.js
+++ static/js/questionAsker.js
@@ -21,9 +21,28 @@ var bgIMAGE_BASE_PATH = "./static/images/bg/";
 
 var charTaskData = [];
 
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+var emailID = getUrlParameter('p');
 // Template for Log Data
+
 var logDataTemplate = {
 	'userID' : '',
+	'emailID' : emailID,
 	'dialogID' : '',
 	'nodeID' : '',
 	'timeStamp' : '',
