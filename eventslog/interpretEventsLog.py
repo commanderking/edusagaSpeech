d77650e7d3a168b5ef412af16a631b0facc43dc9
Jeffrey King <nerdpride4life@gmail.com>
2016-07-19 17:13:06 -0400

    Fix up demoChinese json, fix links on homepage to correct demos, add a few images

---
+++ eventslog/interpretEventsLog.py
@@ -0,0 +1,29 @@
+import sys
+import csv
+import json
+
+eventsLogData = ''
+with open (sys.argv[1], 'r') as my_file:
+	eventsLogData = str(my_file.read())
+	eventsLogData = eventsLogData.encode('utf-8')
+	eventsLogData = eventsLogData.encode('ascii').decode('unicode-escape')
+
+	# Manually unicode and remove it to make it json readable
+	eventsLogData = eventsLogData.replace("u'{", "{")
+	eventsLogData = eventsLogData.replace("}'", "}")
+	
+	jsonFile = json.loads(eventsLogData)
+	print(eventsLogData)
+	'''
+	f = csv.writer(open("test.csv", "wb+"))
+
+f.writerow(["userID", "emailID", "dialogID", "nodeID", "timeStamp", "eventType", "response"])
+
+for x in jsonFile:
+    f.writerow([x["userID"], x["emailID"], x["dialogID"], x["nodeID"], x["eventType"], x["response"]])
+'''
+#print json.loads(eventsLogData)
+
+# Convert data to string 
+# Convert to unicode 
+# print unicode
