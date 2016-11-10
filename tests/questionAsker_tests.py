598f86b032594b3729cafea09ecf090b931c72d8
commanderking <jek582@mail.harvard.edu>
2016-11-10 10:27:33 -0500

    starting to write database tests

--- tests/questionAsker_tests.py
+++ tests/questionAsker_tests.py
@@ -1,6 +1,31 @@
-import os
-import main
+from flask import Flask
+from flask_testing import TestCase
 import unittest
-import tempfile
+from main import app, db
 
-class FlaskrTestCase(unittest.TestCase):
+class MyTest(TestCase):
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/edusagaspeech"
+    TESTING = True
+
+    def create_app(self):
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        return app
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+class AddEpisodes(MyTest):
+    def test_add_episode(self):
+		newEpisode = Episodes('introDavid') 
+		db.session.add(newEpisode)
+		db.session.commit()
+
+		assert newEpisode in db.session
+		response = self.client.get("/")
+		assert user in db.session
+
+if __name__ == '__main__':
+    unittest.main()
