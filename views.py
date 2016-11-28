888a889da25dec5d01602a22198c550da6c947e6
commanderking <jek582@mail.harvard.edu>
2016-11-28 10:48:07 -0500

    update cert

--- views.py
+++ views.py
@@ -64,9 +64,9 @@ def trackVisitorEvent(eventText):
 	except: 
 		pass
 
-@app.route('/.well-known/acme-challenge/iRVuOTsXYkmE171h8NZWsbiDI_hT9kYl1n5_HFUL1VE')
+@app.route('/.well-known/acme-challenge/NQCUjwFGF0qGVXpwRO0hjuovUgcKgQtOL06EnWgJh68')
 def certbot():
-	return 'iRVuOTsXYkmE171h8NZWsbiDI_hT9kYl1n5_HFUL1VE.3LKS5JLsoFTNUAP0BJFtqfW4sEzZ9wUfYgFKWJaL79Q'
+	return 'NQCUjwFGF0qGVXpwRO0hjuovUgcKgQtOL06EnWgJh68.3LKS5JLsoFTNUAP0BJFtqfW4sEzZ9wUfYgFKWJaL79Q'
 
 @app.route('/test')
 def test():
