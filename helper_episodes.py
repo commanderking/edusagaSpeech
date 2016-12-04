eed10759313d593546d14a22f8c7697d661aeb29
commanderking <jek582@mail.harvard.edu>
2016-12-04 17:28:27 -0500

    refactor building public episodes and teacher episodes

--- helper_episodes.py
+++ helper_episodes.py
@@ -14,37 +14,16 @@ def getAllPublicEpisodeData(teacherName):
 	file_list = glob(json_pattern)
 	for file in file_list:
 		# Add episode's data to the sceneMenuData
-		sceneMenuData.append(buildPublicEpisodeData(file, teacherName))
+		sceneMenuData.append(buildEpisodeData(file, teacherName))
 
 	sceneMenuDataStructure["scenes"] = sceneMenuData
 
 	sceneMenuDataStructure = json.dumps(sceneMenuDataStructure, ensure_ascii=False).encode('utf8')
 
-	newFile = open("static/data/teacherScenes/" + teacherName +  ".json", "w")
+	newFile = open("static/data/teacherScenes/" + teacherName + ".json", "w")
 	newFile.write("%s\n" % sceneMenuDataStructure)
 	newFile.close()
 	return sceneMenuData
-
-def buildPublicEpisodeData(jsonPath, teacherName): 
-	with open(jsonPath) as episodeJSON:
-		episodeContent = {}
-		d = json.load(episodeJSON)
-
-		# Add activity name
-		episodeContent['id'] = jsonPath.replace('static/data/public/', '').replace('.json', '')
-		episodeContent['name'] = d['activityName']
-		episodeContent['scenario'] = d['scenario'][0]['text']
-		episodeContent['link'] = jsonPath.replace('static/data/' + teacherName, '/teacher/public/episode').replace('.json', '')
-		episodeContent['tags'] = d['tags']
-		episodeContent['objectives'] = d['objectives']
-		episodeContent['characterImage'] = d['currentImage']
-
-		try: 
-			episodeContent['sequence'] = d['sequence']
-		except:
-			episodeContent['sequence'] = 0
-
-		return episodeContent
 
 def getTeacherEpisodes(username):
 	sceneMenuDataStructure = {}
@@ -87,3 +66,5 @@ def buildEpisodeData(jsonPath, teacherName):
 			episodeContent['sequence'] = 0
 
 		return episodeContent
+
+getAllPublicEpisodeData("public")
