15a8a7e4c1d4d6002902272f3f4c46b421b3141f
commanderking <jek582@mail.harvard.edu>
2016-12-08 10:39:01 -0500

    update before resetting database

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from startup import create_users
 if __name__ == '__main__':
 	db.create_all()
 	teacher = create_users.find_or_create_user('yaoguais2', 'Mypassword1', 'sampleteacher@gmail.com')
-	
 	db.session.add(teacher)
 	db.session.commit()
 
