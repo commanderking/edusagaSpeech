3fe96bbaa0aa1006c1caf83cf553c2c68b4656b2
commanderking <jek582@mail.harvard.edu>
2016-12-08 10:54:20 -0500

    add role_selection in database

--- models.py
+++ models.py
@@ -30,6 +30,7 @@ class User(db.Model, UserMixin):
 
     # User information
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='0')
+    finished_role_selection = db.Column('finished_role_selection', db.Boolean(), default=False)
     first_name = db.Column(db.String(100), nullable=False, server_default='')
     last_name = db.Column(db.String(100), nullable=False, server_default='')
     # user_types are: teacher, student, general
